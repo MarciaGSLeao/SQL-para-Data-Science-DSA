25863fefecb72d4ab19b77ff0c2c68775f6c7ab7
MarciaGSLeao <96493035+MarciaGSLeao@users.noreply.github.com>
2021-12-21 17:55:35 -0300

    Update Query_SQLServer.sql

--- Cap02/Query_SQLServer.sql
+++ Cap02/Query_SQLServer.sql
@@ -14,32 +14,34 @@ create table tb_navios(
 )
 go
 
+
+-- InserÃ§Ã£o em Massa
 bulk insert tb_navios
-from '\\VM-DELL\Users\VM_Dell\Downloads\DADOS_ABERTOS_INSPECAO_NAVIO.csv'  -- Caminho do arquivo no Servidor em que está instalado o SGBD (VM no Dell).
+from '\\VM-DELL\Users\VM_Dell\Downloads\DADOS_ABERTOS_INSPECAO_NAVIO.csv'  -- Caminho do arquivo no Servidor em que estÃ¡ instalado o SGBD (VM no Dell).
 with(
 	format = 'csv',
 	firstrow = 2,
 	fieldterminator = ';',
-	rowterminator = '0x0a'  -- Não consegui usando o valor '\n'. Alternei para '0x0a' e o comando bulk processou. Muuuuuuito estranho. Fonte da pesquisa: 'https://isolution.pro/pt/q/so68956216/sql-server-bcp-bulk-insert-pipe-delimitado-com-arquivo-de-formato-de-qualificador-de-texto'
+	rowterminator = '0x0a'  -- NÃ£o consegui usando o valor '\n'. Alternei para '0x0a' e o comando bulk processou. Muuuuuuito estranho. Fonte da pesquisa: 'https://isolution.pro/pt/q/so68956216/sql-server-bcp-bulk-insert-pipe-delimitado-com-arquivo-de-formato-de-qualificador-de-texto'
 )
 go
 
 select * from tb_navios
 
-/* EXERCÍCIO */
-/* Trabalhando  com  o  mesmo  datasetusado  neste  capítulo, crie  instruções  SQL  que respondam às perguntas abaixo:
+/* EXERCÃCIO */
+/* Trabalhando  com  o  mesmo  datasetusado  neste  capÃ­tulo, crie  instruÃ§Ãµes  SQL  que respondam Ã s perguntas abaixo:
 
-1-Quais embarcações possuem pontuação de risco igual a 310? - 2 registros */
+1-Quais embarcaÃ§Ãµes possuem pontuaÃ§Ã£o de risco igual a 310? - 2 registros */
 select * from tb_navios where pontuacao_risco = 310
 
-/*2-Quais embarcações têm classificação de risco A e índice de conformidade maior ou igual a 95%? - 259 registros */
+/*2-Quais embarcaÃ§Ãµes tÃªm classificaÃ§Ã£o de risco A e Ã­ndice de conformidade maior ou igual a 95%? - 259 registros */
 select * from tb_navios where classificacao_risco = 'A' and indice_conformidade >= 95
 
-/*3-Quais embarcações têm classificação de risco C ou D e índice de conformidade menor ou igual a 95%? - 98 registros */
+/*3-Quais embarcaÃ§Ãµes tÃªm classificaÃ§Ã£o de risco C ou D e Ã­ndice de conformidade menor ou igual a 95%? - 98 registros */
 select * from tb_navios where classificacao_risco in ('C', 'D') and indice_conformidade <= 95
 
-/*4-Quais embarcações têm classificação de risco A ou pontuação de risco igual a 0? - 261 registros */
+/*4-Quais embarcaÃ§Ãµes tÃªm classificaÃ§Ã£o de risco A ou pontuaÃ§Ã£o de risco igual a 0? - 261 registros */
 select * from tb_navios where classificacao_risco = 'A' or pontuacao_risco = 0
 
-/*5-[DESAFIO]Quais embarcações foram inspecionadas em Dezembro de 2016? - 7 registros*/
+/*5-[DESAFIO]Quais embarcaÃ§Ãµes foram inspecionadas em Dezembro de 2016? - 7 registros*/
 select * from tb_navios where temporada like '%Dezembro 2016'
